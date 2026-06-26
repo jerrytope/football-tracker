b9151df98e855485d714c349587e08c7ffde62d6
Ayoade, temitope Jeremiah <61752057+jerrytope@users.noreply.github.com>
2026-06-26 16:00:05 +0100

    aa

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -146,7 +146,10 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = config("AWS_S3_BUCKET_NAME")
-    AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME", default="us-east-1")
+    AWS_S3_ENDPOINT_URL = config("AWS_S3_ENDPOINT_URL")
+    AWS_S3_CUSTOM_DOMAIN = None
+    AWS_DEFAULT_ACL = None
+    AWS_QUERYSTRING_AUTH = True
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     MEDIA_URL = "/media/"
